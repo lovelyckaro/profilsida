c6950cb00e688e6aea2616d0fee6e9c14ba13856
Love Lyckaro <Love Lyckaro>
2017-02-10 22:25:43 +0100

    no clue

--- js/profilsida.js
+++ js/profilsida.js
@@ -1,20 +1,32 @@
 $(document).ready(function(){
     //align banner title to middle of the banner image
     var height = $("#bannerImg").outerHeight(false) / 2 - ($("#bannerTitle").outerHeight(false) / 2) + "px";
-    //$("#bannerTitle").css("top", height);
-    $("#bannerTitle").animate({top: height, opacity: "1"}, 500)
+    $("#bannerTitle").animate({top: height, opacity: "1"}, 500);
+    $("#mainContainer").animate({top: "-100px", opacity: "1"}, 800);
     
     //fade in elements when you scroll to them
     $(window).scroll(function(){
         $(".fadein").each(function(){
-            var elementTop = $(this).offset().top; //Get the element's top most y coordinate
-            var elementBot = elementTop + $(this).outerHeight(true); //Get the element's bottom most y coordinate
+            var elementBot = $(this).offset().top + $(this).outerHeight(true); //Get the element's bottom most y coordinate
             var windowBot = $(window).scrollTop() + $(window).height(); //get the window's bottom most y coordinate
             if(elementBot < windowBot){
-                $(this).animate({"opacity": "1"}, 700);
+                $(this).animate({opacity: "1", top: "0"}, 700);
             }
         });
     });
+    //smooth scrolling when pressing the links
+    $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 60)
+        }, 500);
+        return false;
+      }
+    }
+  });
 });
 
 
