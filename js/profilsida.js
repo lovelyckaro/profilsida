0415498293bdeffa4de047453b74a318ce3ee4e4
Love Lyckaro <Love Lyckaro>
2017-02-10 17:33:02 +0100

    removed unnecessary semicolon

--- js/profilsida.js
+++ js/profilsida.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
     var height = $("#bannerImg").outerHeight(false) / 2 - ($("#bannerTitle").outerHeight(false) / 2);
     $("#bannerTitle").css("top", height);
-    });
 });
 
 
