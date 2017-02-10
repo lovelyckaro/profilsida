d63e559c62fe0002437c592c882f8fce065e9b0e
Love Lyckaro <Love Lyckaro>
2017-02-10 17:51:56 +0100

    started working on fade in animation ass you scroll

--- js/profilsida.js
+++ js/profilsida.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
+    //align banner title to middle of the banner image
     var height = $("#bannerImg").outerHeight(false) / 2 - ($("#bannerTitle").outerHeight(false) / 2);
     $("#bannerTitle").css("top", height);
+    
+    //fade in elements when you scroll to them
+    $(window).scroll(function(){
+        $(".fadein").each(function(){
+            var elementBot = $(this).offset().top + $(this).outerHeight(true); //Get the element's bottom y coordinate
+            var windowBot = $(window).scrollTop() + $(window).height(); //get the window's bottom y coordinate
+            if(elementBot > windowBot){
+                $(this).animate({"opacity": "1"}, 700);
+            }
+        });
+    });
 });
 
 
