ab0d2ba70ed5dd6f1b994f6135b4bd0d543509a0
Love Lyckaro <Love Lyckaro>
2017-02-11 10:57:09 +0100

    tried to add sliding motion to list items

--- js/profilsida.js
+++ js/profilsida.js
@@ -3,11 +3,11 @@ $(document).ready(function(){
     var height = $("#bannerImg").outerHeight(false) / 2 - ($("#bannerTitle").outerHeight(false) / 2) + "px";
     $("#bannerTitle").animate({top: height, opacity: "1"}, 500);
     $("#mainContainer").animate({top: "-100px", opacity: "1"}, 800);
-    
+
     //fade in elements when you scroll to them
     $(window).scroll(function(){
         $(".fadein").each(function(){
-            var elementBot = $(this).offset().top + $(this).outerHeight(true); //Get the element's bottom most y coordinate
+            var elementBot = $(this).offset().top + ($(this).outerHeight(true)/2); //Get the element's middle most y coordinate
             var windowBot = $(window).scrollTop() + $(window).height(); //get the window's bottom most y coordinate
             if(elementBot < windowBot){
                 $(this).animate({opacity: "1", top: "0"}, 700);
@@ -16,17 +16,20 @@ $(document).ready(function(){
     });
     //smooth scrolling when pressing the links
     $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 60)
-        }, 500);
-        return false;
-      }
-    }
-  });
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: (target.offset().top - 60)
+                }, 500);
+                return false;
+            }
+        }
+    });
+    $(".slidelink").click(function(){
+        $(".slidelink").children().slideToggle("slow"); 
+    });
 });
 
 
